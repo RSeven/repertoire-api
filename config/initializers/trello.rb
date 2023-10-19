c21cf1ec6123a4dfbef7f291689fefee469da24b
Ruan <lopes.ruan@gmail.com>
2023-10-19 10:58:16 -0300

    encoding trello credentials

--- config/initializers/trello.rb
+++ config/initializers/trello.rb
@@ -1,6 +1,6 @@
 require 'trello'
 
 Trello.configure do |config|
-    config.developer_public_key = '53a5119ef4dd86fae6ddd61bbd8235e2'
-    config.member_token = '2f651b8aa5d4ec6b70fec3f3493e5e1c919183dceb82237f4b5c2e146605f6b2'
+    config.developer_public_key = Rails.application.credentials.trello.public_key
+    config.member_token = Rails.application.credentials.trello.member_token
 end
